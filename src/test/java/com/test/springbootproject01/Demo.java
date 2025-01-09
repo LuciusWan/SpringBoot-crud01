c89037af13568be514ac5507cc1a973db1d999a9
LuciusWan <18099488938@163.com>
2025-01-09 22:40:30 +0800

    Add more project files

--- src/test/java/com/test/springbootproject01/Demo.java
+++ src/test/java/com/test/springbootproject01/Demo.java
@@ -14,8 +14,7 @@ public class Demo {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-/*       
-        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);*/
+/*       */
         EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "web-tlias-lucius";
